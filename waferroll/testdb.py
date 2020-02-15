9ad66f61a872a02bad6652836d5014b5c18204f0
allen <allen.liu@tpgtelecom.com.au>
2020-02-15 23:52:22 +1100

    change cicd pipeline to master only

--- waferroll/testdb.py
+++ waferroll/testdb.py
@@ -7,9 +7,7 @@ from TestModel.models import Test
 
 # 数据库操作
 def testdb(request):
-    # 初始化
-    response = ""
-    response1 = ""
 
     # 通过objects这个模型管理器的all()获得所有数据行，相当于SQL中的SELECT * FROM
     list = Test.objects.all()
+    return HttpResponse("<p>" + list + "</p>")
